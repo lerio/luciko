8ebaf6cee188b73fd557be80ec88ed40459ac561
lerio <valerio.donati@gmail.com>
2026-02-09 21:44:40 +0100

    Refactor code structure for improved readability and maintainability

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -5,5 +5,6 @@ export const TARGET_CHAT_ID = 'c1';
 export const TARGET_CHAT: Chat = {
     id: TARGET_CHAT_ID,
     name: 'Luciana Milella',
-    isGroup: false
+    isGroup: false,
+    avatarUrl: '/assets/luciko.jpg'
 };
